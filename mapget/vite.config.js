0bef16cbad0c6f223eb8433999a52d0524cfc906
Enes <eatobb@gmail.com>
2026-04-07 01:37:36 +0200

    as

--- mapget/vite.config.js
+++ mapget/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "app",
   base: "./",
+  publicDir: "public",
   plugins: [react()],
+  build: {
+    outDir: "../",
+    emptyOutDir: false,
+  },
 });
